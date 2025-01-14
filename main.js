f1f855cb66a5465e7544ddb41d84c4b18bf128a3
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-01-13 19:31:10 -0800

    operate function created

--- main.js
+++ main.js
@@ -14,6 +14,27 @@ function divide(num1, num2)
 {
     return num1 / num2;
 }
-let num1;
-let num2;
+
+function operate(choice)
+{
+    if (choice == 0)
+    {
+        add(num1, num2);
+    }
+    else if (choice == 1)
+    {
+        subtract(num1, num2);
+    }
+    else if (choice == 2)
+    {
+        multiply(num1, num2);
+    }
+    else if (choice == 3)
+    {
+        divide(num1, num2);
+    }
+}
+const firstNum = document.createElement("p")
+const secondNum = document.createElement("p")
 let operator;
+
