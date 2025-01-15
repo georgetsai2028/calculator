9922c55dff4ee1f64e0568c74c8508e110442e09
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-01-14 18:43:42 -0800

    numbers added need to add operator buttons

--- main.js
+++ main.js
@@ -62,16 +62,22 @@ calculatorResultContainer.style.border = "4px solid black";
 calculatorResultContainer.textContent = "RESULTS";
 const operaterButtonsContainer = document.getElementById("operatorButtons");
 operaterButtonsContainer.style.display = "flex";
+operaterButtonsContainer.style.height = "50px";
 const numberButtonContainer = document.getElementById("numberButtons");
 numberButtonContainer.style.display = "flex";
-
+numberButtonContainer.style.height = "50px";
+numberButtonContainer.style.backgroundColor = "blue";
+numberButtonContainer.style.padding = "30px";
+numberButtonContainer.style.margin = "10px";
+numberButtonContainer.style.flexWrap = "wrap";
 for (let i = 0; i < 9; i++)
     {
         const numbersOneToNine = document.createElement("div");
         numbersOneToNine.textContent = i + 1;
-        numbersOneToNine.style.width = "16px";
-        numbersOneToNine.style.height = "16px";
-        numbersOneToNinestyle.display = "flex";
+        numbersOneToNine.style.width = "25%";
+        numbersOneToNine.style.height = "25%";
+        numbersOneToNine.style.padding = "16px";
+        numbersOneToNine.style.display = "flex";
         numbersOneToNine.style.alignItems = "center";
         numbersOneToNine.style.justifyContent = "center";
         numberButtonContainer.appendChild(numbersOneToNine);
